96f0595f0a26976b1393172c1c6326d4071636e0
noko516 <220516@jc-21.jp>
2022-12-07 13:45:08 +0900

    まあまあ

--- kadai1/staff_togo.php
+++ kadai1/staff_togo.php
@@ -3,9 +3,9 @@
     <head>
         <meta charset="UTF-8">
         <title>課題１</title>
+        <link href="staff_togo.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-    <link rel="stylesheet" href="staff_togo.css">
        <?php 
        try{
         $dsn = 'mysql:dbname=shop;host=localhost;cherset=utf8';
@@ -21,26 +21,28 @@
        
        print 'スタッフ一覧<br /><br />';
 
-       print '<form method="post"action="staff_bran.php">';
+       print '<form method="post"action="staff_bran.php">' ."\n";
        while(true)
        {
         $rec=$stmt->fetch(PDO::FETCH_ASSOC);
         if($rec==false){
               break;
         }
-        print '<input type="radio"name="staffcode"value="'.$rec['ID'].'">';
+        echo "<div class=kakoi>";
+        print '<input type="radio"name="staffcode"value="'.$rec['ID'].'">' ."\n";
         print $rec['ID'];
         print $rec['simei'];
         print $rec['hurigana'];
         print'<br />';
+        echo "</div>";
        }
 
-      
-       echo  '<input type="submit" name="sansyo" value="参照">';
-       echo  '<input type="submit" name="sinki" value="追加">';
-       echo  '<input type="submit" name="syusei" value="修正">';
-       echo  '<input type="submit" name="sakuzyo" value="削除">';
-       print '</from>';
+       echo  '<input class="ao" type="submit" name="sansyo" value="参照">' ."\n";
+       echo  '<input class="ao" type="submit" name="sinki" value="追加">' ."\n";
+       echo  '<input class="ao" type="submit" name="syusei" value="修正">' ."\n";
+       echo  '<input class="ao" type="submit" name="sakuzyo" value="削除">' ."\n";
+       print '</from>' ."\n";
+
       
        }
        catch(Exception $e){
