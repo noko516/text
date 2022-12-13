5fd871ea9988921d8fd9a8b02fc40097b3c0393a
noko516 <220516@jc-21.jp>
2022-12-13 14:49:47 +0900

    手直し

--- kadai1/staff_syusei.php
+++ kadai1/staff_syusei.php
@@ -42,20 +42,29 @@
     <br />
     <form method="post" action="staff_syusei_check.php">
         <input type="hidden" name="code" value="<?= $staff_code; ?>">
-        スタッフ名<br />
-        <input type="text" name="name" style="width:200px" value="<?php print $staff_name; ?>"><br />
-        ふりがな<br />
-        <input type="text" name="name2" style="width:200px"><br />
-        郵便番号<br />
-        <input type="number" name="bango" style="width:100px"><br />
-        住所<br />
-        <input type="text"name="zyusyo" style="width:200px"><br />
-        電話番号<br />
-        <input type="number" name="tel" style="width:200px"><br />
-        E-メールアドレス<br />
-        <input type="email" name="mail" style="width:200px"><br />
-        <br />
-
+        <table>
+        <tbody>
+        <tr>
+        <th>スタッフ名</th><td><input type="text" name="name" style="width:200px" value="<?php print $staff_name; ?>"></td>
+        </tr>
+        <tr>
+        <th>ふりがな</th><td><input type="text" name="name2" style="width:200px"></td>
+        </tr>
+        <tr>
+        <th>郵便番号</th><td><input type="number" name="bango" style="width:100px"></td>
+        </tr>
+        <tr>
+        <th>住所</th><td><input type="text"name="zyusyo" style="width:200px"></td>
+        </tr>
+        <tr>
+        <th>電話番号</th><td><input type="number" name="tel" style="width:200px"></td>
+       </tr>
+        <tr>
+        <th>E-メールアドレス</th><td><input type="email" name="mail" style="width:200px"></td>
+        </tr>
+       </tbody>
+       </table> 
+       <br />
         <button type="button" onclick="history.back()">戻る</button>
         <button type="submit">OK</button>
     </form>
